ff695ab4384bb895f5dfab8b65d83db31a2510d3
chadmv95 <vredevch@mail.gvsu.edu>
2018-07-11 16:26:54 -0400

    overloaded add function, added comments

--- BPlusTree.java
+++ BPlusTree.java
@@ -3,26 +3,86 @@ import java.util.Scanner;
 public class BPlusTree {
 	private static Node root;
 
+	/*
+	 * creates root node with given data
+	 */
     public BPlusTree(String data)
     {
         root = new Node(data);
     }
-
-    public void add(Node parent, Node child, int childNum)
+    
+    /*
+     * creates root node with no data
+     */
+    public BPlusTree()
     {
-    	
-        parent.setChildren(child, childNum);
+    	root = new Node("");
+    }
+
+    /*
+     * Add a node to the tree underneath the selected parent
+     * at the selected position
+     * 
+     * returns true on successful add
+     * returns false if child is null
+     */
+    public boolean add(Node parent, Node child, int childNum)
+    {
+        if(child == null) 
+        	return false;
         
+    	parent.setChildren(child, childNum);
+        return true;
+    }
+    
+    /*
+     * Add node to the soonest available spot
+     * 
+     * returns true upon successful add
+     * returns false upon unsuccessful add & child is not added
+     */
+    public boolean add(Node parent, Node child)
+    {
+    	if(child != null)
+	    	for(int i=0; i<5; i++) 
+	    		if(parent.getChildren(i) == null) {
+	    			parent.setChildren(child, i);
+	    			return true;
+	    		}
+	    	
+	    
+    	return false;
+	    	
+    }
+    
+    /*
+     * Delete child from the tree
+     * Note: children under the deleted node will also be removed
+     * 
+     * returns true upon deletion
+     * returns false if parent is null
+     */
+    public boolean delete(Node parent, int childNum)
+    {    	
+    	if(parent == null) 
+    		return false;
+    	
+    	parent.setChildren(null, childNum);
+    	return true;
     }
     
     /*
      * This function needs work and may need to be reconsidered
      * if we decide to use the website for the GUI
+     * 
+     * returns true upon successful print
+     * returns false if tree is empty
      */
-    public void printTree() {
-    	if(root == null) return;
+    public boolean printTree() {
+    	if(root == null)
+    		return false;
     	
-    	return;
+    	return true;
     }
 
     public static void main(String args[])
@@ -47,6 +107,8 @@ public class BPlusTree {
         
         Node u1 = new Node(userString);
         tree.add(root, u1, userInt);
+        
+        input.close();
     	
     }
 
